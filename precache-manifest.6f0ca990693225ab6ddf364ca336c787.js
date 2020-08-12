b4fd5fb3e6e0c3b992b4dab89ca8e30401548a45
Ankit290801 <tlk2rkrab@gmail.com>
2020-08-13 00:14:48 +0530

    Updates

---
+++ precache-manifest.6f0ca990693225ab6ddf364ca336c787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1308d3d8a3a7d12a6ba5e33241b05420",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "a5313c149ed7c07b5304",
+    "url": "/deploy-react/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "bd22a3d272ba66694c2e",
+    "url": "/deploy-react/static/js/2.ce202010.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.ce202010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5313c149ed7c07b5304",
+    "url": "/deploy-react/static/js/main.e2b1b1c1.chunk.js"
+  },
+  {
+    "revision": "319debf7c9adc44e6642",
+    "url": "/deploy-react/static/js/runtime-main.e1820e13.js"
+  }
+]);
